2db342d25a99661f99f8ad27e8d8879c407b7d41
SmartWalle <smartwalle@gmail.com>
2017-07-05 08:23:24 +0800

    update

--- model.go
+++ model.go
@@ -1,21 +1,5 @@
 package odin
 
-const (
-	k_ODIN_GROUP_TYPE_PERMISSION = "1" // 权限组
-	k_ODIN_GROUP_TYPE_ROLE       = "2" // 角色组
-)
-
-// odin_gps_[type] 用于存储 Group 的 Id 列表
-
-// Group 用于存储分组信息，不参与权限的控制
-// 特殊限定：创建之后，只能修改 name，不能修改 type
-// Key：odin_gp_[id]
-type Group struct {
-	Id   string `json:"id"     redis:"id"`   // id
-	Type string `json:"type"   redis:"type"` // 组类型：1、权限组；2、角色组
-	Name string `json:"name"   redis:"name"` // 组的名字
-}
-
 // Permission 用于存储权限信息
 // Key：odin_pn_[id]
 type Permission struct {
